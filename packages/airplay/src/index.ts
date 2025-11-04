cec4b4b56c6919f373c5bc5382c1fcd147d3e0f7
Bas Milius <basmilius@hotmail.com>
2025-11-04 20:02:56 +0100

    feat(airplay): expose airplay library.

---
+++ packages/airplay/src/index.ts
@@ -0,0 +1,4 @@
+export type { AirPlayDataStream, AirPlayDataStreamMessages, AirPlayPairing, AirPlayRTSP, AirPlayStream, AirPlayVerify } from './protocol';
+export { AirPlay } from './protocol';
+
+export * as Proto from './proto';
